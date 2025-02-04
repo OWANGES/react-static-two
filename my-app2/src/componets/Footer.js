f6ccf01ae5c9111f0b95b154d47ea9e9b36cb0cc
OWANGES <omnikar11@gmail.com>
2025-02-04 18:06:26 -0500

    edit 2

--- my-app2/src/componets/Footer.js
+++ my-app2/src/componets/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 function MyFooter(){
     return (
         <div>
-            <h3>My Footer</h3>
+            <h3>The lesson of the story is hard work pays off</h3>
         </div>
 );}
 
